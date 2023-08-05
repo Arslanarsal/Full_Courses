9ae4ea43656ec8b9899a09c45f12e6964fa601c6
Arslan <arslanarsal455@gmail.com>
2023-08-05 19:25:22 +0500

    Quick and merge sort

---
+++ Apna College C++/Quick_Sort.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time Complexity is O(n^2) in worst case
+// Time Complexity is O(nlogn) in best case
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
+    do
+    {
+        while (arr[i] <= pivot)
+        {
+            i++;
+        }
+        while (arr[j] > pivot)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+    } while (i < j);
+    arr[low] = arr[j];
+    arr[j] = pivot;
+    return j;
+}
+
+
+void quickSort(int arr[], int start, int end)
+{
+    int piIdx;
+    if (start < end)
+    {
+        piIdx = partition(arr, start, end);
+        quickSort(arr, start, piIdx - 1);
+        quickSort(arr, piIdx + 1, end);
+    }
+}
+
+void printArray(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int n = 5; // sizeof(array) / sizeof(array[0]);
+    printArray(array, n);
+    quickSort(array, 0, n - 1);
+    printArray(array, n);
+
+    return 0;
+}
