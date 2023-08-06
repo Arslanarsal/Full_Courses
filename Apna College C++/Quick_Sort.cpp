00ba9dbb0248c8d762c5e13e7ad719c158e87e05
Arslan <arslanarsal455@gmail.com>
2023-08-06 18:49:35 +0500

    Modified quick sort

--- Apna College C++/Quick_Sort.cpp
+++ Apna College C++/Quick_Sort.cpp
@@ -4,33 +4,66 @@ using namespace std;
 // Time Complexity is O(n^2) in worst case
 // Time Complexity is O(nlogn) in best case
 
+
+// -------------->>>>>>>>>>>>>>>Pivot is the Starting element
+// int partition(int arr[], int low, int high)
+// {
+//     int pivot = arr[low];
+//     int i = low + 1;
+//     int j = high;
+//     do
+//     {
+//         while (arr[i] <= pivot)
+//         {
+//             i++;
+//         }
+//         while (arr[j] > pivot)
+//         {
+//             j--;
+//         }
+//         if (i < j)
+//         {
+//             int temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+//         }
+
+//     } while (i < j);
+//     arr[low] = arr[j];
+//     arr[j] = pivot;
+//     return j;
+// }
+
+
+
+
+// -------------->>>>>>>>>>>>>>>Pivot is the Starting element
+
 int partition(int arr[], int low, int high)
 {
-    int pivot = arr[low];
-    int i = low + 1;
-    int j = high;
-    do
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
     {
-        while (arr[i] <= pivot)
+        if (arr[j] <= pivot)
         {
             i++;
-        }
-        while (arr[j] > pivot)
-        {
-            j--;
-        }
-        if (i < j)
-        {
+
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
         }
+    }
 
-    } while (i < j);
-    arr[low] = arr[j];
-    arr[j] = pivot;
-    return j;
+
+    arr[high] = arr[i + 1];
+    arr[i + 1] = pivot;
+    return i + 1;
 }
+
+
+
 
 
 void quickSort(int arr[], int start, int end)
@@ -39,6 +72,7 @@ void quickSort(int arr[], int start, int end)
     if (start < end)
     {
         piIdx = partition(arr, start, end);
+
         quickSort(arr, start, piIdx - 1);
         quickSort(arr, piIdx + 1, end);
     }
@@ -53,11 +87,10 @@ void printArray(int a[], int n)
     cout << endl;
 }
 
-
 int main()
 {
-    int array[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int n = 5; // sizeof(array) / sizeof(array[0]);
+    int array[] = {9, 8, 5 , 7};
+    int n = 4; // sizeof(array) / sizeof(array[0]);
     printArray(array, n);
     quickSort(array, 0, n - 1);
     printArray(array, n);
