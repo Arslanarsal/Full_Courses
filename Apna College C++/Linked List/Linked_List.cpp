6dd19441e89a8aff48262cbe96bab712c57c25c1
Arslan <arslanarsal455@gmail.com>
2023-08-12 21:31:06 +0500

    Linked list

---
+++ Apna College C++/Linked List/Linked_List.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+    }
+};
+
+void insertAtTail(node* &head, int val)
+{
+    node* n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node* temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+    
+}
+
+void display(node* head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << "->";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+    return;
+}
+
+int main()
+{
+    node* head  = NULL;
+    insertAtTail(head , 1);
+    insertAtTail(head , 2);
+    insertAtTail(head , 3);
+    insertAtTail(head , 4);
+    display(head);
+
+    return 0;
+}
