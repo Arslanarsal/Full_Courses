eae69383e1eba4478590036469994a214b20acc3
Arslan <arslanarsal455@gmail.com>
2024-03-20 00:41:59 +0500

    Changing in react project

---
+++ React js/my-app/src/App.js
@@ -0,0 +1,11 @@
+
+import Fn from './Test'
+
+
+function App() {
+  return (
+    <Fn/>
+  );
+}
+
+export default App;
---
+++ React js/my-app/src/Test.js
@@ -0,0 +1,6 @@
+
+function Fn() {
+    return <h1>Uog is best university</h1>
+}
+
+export default Fn;
