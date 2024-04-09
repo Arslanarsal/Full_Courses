1a9d4d5694adbfadc6c3a821567cb1ff1c1cad6a
Arslan <arslanarsal455@gmail.com>
2024-04-10 01:46:14 +0500

    react tailwind css

--- React js/Counter/src/App.jsx
+++ React js/Counter/src/App.jsx
@@ -1,38 +1,32 @@
-import { useState } from 'react'
+// import { useState } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
   // let counter = 15;
-  let [counter, setcounter] = useState(15);
+  // let [counter, setcounter] = useState(15);
 
-  const addValue = () => {
-    if (counter + 1 < 21) {
-      setcounter(counter + 1);
-    }
-  }
-  const removeValue = () => {
-    if (counter - 1 > -1) {
-      setcounter(counter - 1);
-    }
-
-  }
+  // const addValue = () => {
+  //   if (counter + 1 < 21) {
+  //     setcounter(counter + 1);
+  //   }
+  // };
+  // const removeValue = () => {
+  //   if (counter - 1 > -1) {
+  //     setcounter(counter - 1);
+  //   }
+  // };
 
   return (
     <>
       <h1>Vite + React</h1>
-      <h2>Counter : {counter}</h2>
-      <button onClick={addValue} >
-        Add Value: {counter}
-      </button>
-      <br />
-      <button
-        onClick={removeValue} >
-        Remove Value: {counter}
-      </button>
-    </>
-  )
-}
 
-export default App
+      {/* <h2>Counter : {counter}</h2>
+      <button onClick={addValue}>Add Value: {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value: {counter}</button> */}
+    </>
+  );
+}
+export default App;
---
+++ React js/props tailwind/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import "./App.css";
+import Card from "./Component/Card";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="flex bg-green-600  p-6">Hello World</h1>
+      <Card Username="bbbbbbbbbb" />
+      <Card />
+    </>
+  );
+}
+
+export default App;
