3c58f0e68c9f5c577287346c371f17df54e7f581
Arslan <arslanarsal455@gmail.com>
2024-07-28 23:23:29 +0500

    backend

--- Backend/Backend with Sheryians/Node js and npm with File System/Node.js_and_npm.js
+++ Backend/Backend with Sheryians/Node js and npm with File System/Node.js_and_npm.js
@@ -1,5 +1,6 @@
 const { log } = require('node:console');
 const fs = require('node:fs');
+const http = require('http')
 
 // fs.writeFile("first.txt" , "I any write anything in this file" , function(err){
 //     if (err) {
@@ -20,11 +21,47 @@ const fs = require('node:fs');
 //     }
 // })
 
-fs.rename("first.txt"  , "reName.txt"  , function(err){
-if (err) {
-    console.log(err);
-}
-else{
-    console.log("Done Rename");
-}
-})
+// fs.rename("first.txt"  , "reName.txt"  , function(err){
+// if (err) {
+//     console.log(err);
+// }
+// else{
+//     console.log("Done Rename");
+// }
+// })
+
+// fs.copyFile("reName.txt", "./Copyfile.txt", function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Done");
+//     }
+// })
+
+
+// fs.unlink("Copyfile.txt", function(err){
+//     if (err) {
+//         console.log(err.message);
+//     }else{
+//         console.log("Done");
+//     }
+// })
+
+
+// fs.readFile("reName.txt",'utf8', function (err, data) {
+//     if (err) {
+//         console.log(err);
+
+//     } else {
+//         console.log(data);
+//     }
+// })
+
+// const server = http.createServer(function (req, res) {
+//     res.end("Hello world");
+// })
+
+// server.listen(3000);
+
+
+console.log("Hey! Hello ");
