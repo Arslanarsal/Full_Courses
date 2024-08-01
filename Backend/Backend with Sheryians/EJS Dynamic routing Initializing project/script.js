59dbfee32435f3c16a4eb07e2dbd05ebba39309e
Arslan <arslanarsal455@gmail.com>
2024-08-01 20:55:41 +0500

    backend

---
+++ Backend/Backend with Sheryians/EJS Dynamic routing Initializing project/script.js
@@ -0,0 +1,20 @@
+// const express = require("express");
+const { log } = require("console");
+const path = require("path");
+// const app = express();
+console.log('\n');
+console.log((path.join(__dirname, 'public')));
+console.log('\n');
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.static(path.join('__dirname', 'public')));
+// app.set('view engine', 'ejs');
+
+// app.get("/", function (req, res) {
+//     res.render("index");
+// })
+
+// app.listen(3000, () => {
+//     console.log("Is Runing");
+// })
