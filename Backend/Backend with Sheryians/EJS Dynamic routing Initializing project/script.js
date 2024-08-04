c0ec8c3149ad27ca8bba35f354c33b16b675aa91
Arslan <arslanarsal455@gmail.com>
2024-08-04 22:13:57 +0500

    add thing ready

--- Backend/Backend with Sheryians/EJS Dynamic routing Initializing project/script.js
+++ Backend/Backend with Sheryians/EJS Dynamic routing Initializing project/script.js
@@ -1,20 +1,23 @@
-// const express = require("express");
+const express = require("express");
 const { log } = require("console");
 const path = require("path");
-// const app = express();
-console.log('\n');
-console.log((path.join(__dirname, 'public')));
-console.log('\n');
+const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.static(path.join('__dirname', 'public')));
-// app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
-// app.get("/", function (req, res) {
-//     res.render("index");
-// })
+app.get("/", function (req, res) {
+  res.render("index");
+});
 
-// app.listen(3000, () => {
-//     console.log("Is Runing");
-// })
+app.get("/profile/:username", function (req, res) {
+  //   res.render("index");
+  //   res.send("chal arah h ");
+  res.send(`Welcome, ` + req.params.username);
+});
+
+app.listen(3000, () => {
+  console.log("Is Runing");
+});
