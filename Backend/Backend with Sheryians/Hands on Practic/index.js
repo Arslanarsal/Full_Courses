e011223689971f27dfbdef5a7f652c69d09ab18f
Arslan <arslanarsal455@gmail.com>
2024-11-26 17:11:56 +0500

    bug fix

--- Backend/Backend with Sheryians/Hands on Practic/index.js
+++ Backend/Backend with Sheryians/Hands on Practic/index.js
@@ -16,28 +16,27 @@ app.get("/", function (req, res) {
 });
 
 
-// app.get("/files/:filename", function (req, res) {
-//   fs.readFile(`./files/${req.params.filename}`, "utf-8", function (err, filedata) {
-//     if (err) {
-//       console.log("File reading error");
-//     }
-//     else {
-//       res.render("file", { filename: req.params.filename, filedata: filedata })
-
-//     }
-//   })
-// });
-
+app.get("/files/:filename", function (req, res) {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", function (err, filedata) {
+    if (err) {
+      console.log("File reading error");
+    }
+    else {
+      res.render("file", { filename: req.params.filename, filedata: filedata })
+    }
+  })
+});
 
 
-// app.post(`/create`, function (req, res) {
-//   fs.writeFile(
-//     `./files/${req.body.title.split(" ").join("")}.txt`, req.body.details,
-//     function (err) {
-//       res.redirect("/");
-//     }
-//   );
-// });
+
+app.post(`/create`, function (req, res) {
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`, req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
 
 app.listen(3000, () => {
   console.log("Server is runing on port 3000");
--- Backend/Backend with Sheryians/Node js and npm with File System/Node.js_and_npm.js
+++ Backend/Backend with Sheryians/Node js and npm with File System/Node.js_and_npm.js
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
-const { log } = require('node:console');
-const fs = require('node:fs');
-const http = require('http')
-
-fs.writeFile("first.txt" , "I any write anything in this file" , function(err){
-    if (err) {
-        console.error(err);
-    }
-    else{
-        console.log("Done");
-    }
-})
-=======
 const { log } = require("node:console");
 const fs = require("node:fs");
 const http = require("http");
->>>>>>> 40ffb59c2c823560ef8db2937c3e5d29ec72bd2e
 
 fs.writeFile("first.txt", "I any write anything in this file", function (err) {
   if (err) {
@@ -51,33 +36,17 @@ fs.copyFile("reName.txt", "./Copyfile.txt", function (err) {
     }
 })
 
-<<<<<<< HEAD
-
 fs.unlink("Copyfile.txt", function(err){
     if (err) {
         console.log(err.message);
     }else{
         console.log("Done");
-    }
+    } 
 })
-
 
 fs.readFile("reName.txt",'utf8', function (err, data) {
     if (err) {
         console.log(err);
-=======
-// fs.unlink("Copyfile.txt", function(err){
-//     if (err) {
-//         console.log(err.message);
-//     }else{
-//         console.log("Done");
-//     }
-// })
-
-// fs.readFile("reName.txt",'utf8', function (err, data) {
-//     if (err) {
-//         console.log(err);
->>>>>>> 40ffb59c2c823560ef8db2937c3e5d29ec72bd2e
 
     } else {
         console.log(data);
@@ -90,9 +59,4 @@ const server = http.createServer(function (req, res) {
 
 // server.listen(3000);
 
-<<<<<<< HEAD
-
-// console.log("Hey! Hello ");
-=======
 console.log("Hey! Hello ");
->>>>>>> 40ffb59c2c823560ef8db2937c3e5d29ec72bd2e
